2d0811809651d1617faa840bd94adf7f9eb5b134
Omar Al-Terawi <omar_alterawi@hotmail.com>
2023-06-15 22:50:08 +0300

    UserServices Migration

--- Eihal/Data/Entites/UserServices.cs
+++ Eihal/Data/Entites/UserServices.cs
@@ -6,16 +6,16 @@ namespace Eihal.Data.Entites
     public class UserServices
     {
         public int Id { get; set; }
-        public Guid UserId { get; set; }
+        public int UserId { get; set; }
         [ForeignKey(nameof(Service))]
         public int ServiceId { get; set; }
         public Services Service { get; set; }
-        public string TitleAr { get; set; }
-        public string TitleEn { get; set; }
+        public string? TitleAr { get; set; }
+        public string? TitleEn { get; set; }
         public ServicesStatusEnum Status { get; set; }
         public double Fee { get; set; }
         public double Price { get; set; }
-        public string RejectionReason { get; set; }
+        public string? RejectionReason { get; set; }
         public Guid? CreatedBy { get; set; }
         public DateTime? CreatedOn { get; set; }
         public Guid? UpdatedBy { get; set; }
---
+++ Eihal/Data/Migrations/20230615194642_UpdateUserServices.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eihal.Data.Migrations
+{
+    public partial class UpdateUserServices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropColumn(
+         name: "UserId",
+         table: "UserServices");
+
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "UserServices",
+                type: "int",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TitleEn",
+                table: "UserServices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TitleAr",
+                table: "UserServices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RejectionReason",
+                table: "UserServices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "UserServices",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TitleEn",
+                table: "UserServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TitleAr",
+                table: "UserServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RejectionReason",
+                table: "UserServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
